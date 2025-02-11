bf7d7a90f2d4692b413fb830736514d292090976
Luis <luisgmgomes@gmail.com>
2025-02-11 17:02:12 -0300

    feat: first commit

---
+++ src/main/java/gym/murasaki/MurasakiGym/MurasakiGymApplication.java
@@ -0,0 +1,13 @@
+package gym.murasaki.MurasakiGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurasakiGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurasakiGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gym/murasaki/MurasakiGym/MurasakiGymApplicationTests.java
@@ -0,0 +1,13 @@
+package gym.murasaki.MurasakiGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurasakiGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
